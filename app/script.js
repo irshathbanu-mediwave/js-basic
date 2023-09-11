f3fadef50f492f36f4a5579eb705dba78d7f1f9d
irshathbanu-mediwave <irshathbanu@mindwaveventures.com>
2023-09-11 14:56:43 +0530

    add the button function

--- app/script.js
+++ app/script.js
@@ -1,49 +1,49 @@
 let favfruitname = [
   {
     id: "1",
-    name: "Sweetsop",
+    names: "Sweetsop",
     kg: "1kg",
   },
   {
     id: "2",
-    name: "Finger lime",
+    names: "Finger lime",
     kg: "2kg",
   },
   {
     id: "3",
-    name: "Passion fruit",
+    names: "Passion fruit",
     kg: "3kg",
   },
   {
     id: "4",
-    name: "Tangerine",
+    names: "Tangerine",
     kg: "4kg",
   },
   {
     id: "5",
-    name: "Hog plum",
+    names: "Hog plum",
     kg: "5kg",
   },
 
   {
     id: "7",
-    name: "	Ice apple",
+    names: "	Ice apple",
     kg: "7kg",
   },
 
   {
     id: "8",
-    name: "Cherimoya",
+    names: "Cherimoya",
     kg: "8kg",
   },
   {
     id: "9",
-    name: "Jackfruit",
+    names: "Jackfruit",
     kg: "4kg",
   },
   {
     id: "10",
-    name: "Nectarine",
+    names: "Nectarine",
     kg: "10kg",
   },
 ];
@@ -62,9 +62,19 @@ function Fruitnamediv(fruits){
 
     const h3=document.createElement('h3')
     h3.innerText=fruits['kg'];
+
+    const button=document.createElement('button')
+    button.setAttribute('id','delete-div')
      
     div.appendChild(h2)
     div.appendChild(h3)
+    div.appendChild(button)
+    
+    for(let i=0; i<favfruitname; i++)
+    {
+        let result=`${favfruitname[i]['id']} -${favfruitname[i]['names'] } -${favfruitname[i]['kg']}`
+         console.log(result);
+    }
 
 }
 
